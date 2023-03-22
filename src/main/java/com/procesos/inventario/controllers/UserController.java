44f610f78c19c31d8e2f773c1a0355d2c09a4da8
Cristo Carrillo <cjcarrillos@ufpso.edu.co>
2023-03-21 20:54:23 -0500

    ➕ ADD: validation and code response

--- src/main/java/com/procesos/inventario/controllers/UserController.java
+++ src/main/java/com/procesos/inventario/controllers/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -18,29 +20,53 @@ public class UserController {
     private final UserServiceImp userServiceImp;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userServiceImp.getUser(id));
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<Void> saveUser(@RequestBody User user) {
-        if (!userServiceImp.createUser(user)) {
-            return new ResponseEntity<Void>(HttpStatus.PAYMENT_REQUIRED);
+    public ResponseEntity findUserById(@PathVariable Long id) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            return new ResponseEntity<>(userServiceImp.getUser(id), HttpStatus.OK);
+        } catch (Exception e) {
+            response.put("status", "404");
+            response.put("message", "No se encontro el usuario!");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<User>> getAllUser() {
-        return ResponseEntity.ok(userServiceImp.allUser());
+    @PostMapping("")
+    public ResponseEntity saveUser(@RequestBody User user) {
+        Map<String, String> response = new HashMap<>();
+        if (!userServiceImp.createUser(user)) {
+            response.put("status", "400");
+            response.put("message", "Error en los datos enviados");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+        response.put("status", "201");
+        response.put("message", "Usuario creado con éxito");
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("")
+    public ResponseEntity getAllUser() {
+        Map<String, String> response = new HashMap<>();
+        List<User> registeredUsers = userServiceImp.allUser();
+        if (registeredUsers.isEmpty()) {
+            response.put("status", "404");
+            response.put("message", "No hay usuarios");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(registeredUsers);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateUser(@PathVariable Long id, @RequestBody User user){
-        if (!userServiceImp.updateUser(id,user)) {
-            return new ResponseEntity<Void>(HttpStatus.PAYMENT_REQUIRED);
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody User user) {
+        Map<String, String> response = new HashMap<>();
+        if (!userServiceImp.updateUser(id, user)) {
+            response.put("status", "400");
+            response.put("message", "Error en los datos enviados");
+            return new ResponseEntity<>(response, HttpStatus.PAYMENT_REQUIRED);
         }
-        return new ResponseEntity<Void>(HttpStatus.CREATED);
+        response.put("status", "200");
+        response.put("message", "Usuario actualizado con éxito");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
